72e5261c66b461a7154d97ad308ff56730c7eed4
Einars <luckygpmaker@gmail.com>
2023-10-12 15:12:58 +0200

    Created

---
+++ ATM V2/ATM V2/Cardholder.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_V2
+{
+    internal class Cardholder
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Pin { get; set; }
+        public double Balance { get; set; }
+        /// <summary>
+        /// Get Balance from account
+        /// </summary>
+        public void getBalance()
+        {
+            Console.Write("Your Balance is: ");
+            Console.WriteLine(Balance);
+        }
+        /// <summary>
+        /// Deposits Money to account
+        /// </summary>
+        public void doDeposit()
+        {
+            Console.Clear();
+            Console.Write("How much would you like to Deposit: ");
+            string input = Console.ReadLine();
+            int value = Convert.ToInt32(input);
+
+            Balance = value + Balance;
+        }
+        /// <summary>
+        /// Withdraws money from account
+        /// </summary>
+        public void doWithdraw()
+        {
+
+            Console.Clear();
+            getBalance();
+            if(Balance > 0)
+            {
+
+                
+                Console.WriteLine("\n1# 1000Kr");
+                Console.WriteLine("2# 500Kr");
+                Console.WriteLine("3# 200Kr");
+                Console.WriteLine("4# Custom Amount");
+                Console.Write("\nInput :");
+
+                int Choice = int.Parse(Console.ReadLine());
+                
+
+                if (Choice == 1)
+                {
+                    int value = 1000;
+                    if(value !< Balance)
+                    {
+                        Console.Clear();
+                        Balance = Balance - value;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You dont have enough balance to Withdraw: " + value);
+                    }
+                }
+                else if (Choice == 2)
+                {
+                    int value = 500;
+                    if (value !< Balance)
+                    {
+                        Console.Clear();
+                        Balance = Balance - value;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You dont have enough balance to Withdraw: " + value);
+                    }
+                }
+                else if (Choice == 3)
+                {
+                    int value = 200;
+                    if (value !< Balance)
+                    {
+                        Console.Clear();
+                        Balance = Balance - value;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You dont have enough balance to Withdraw: " + value);
+                    }
+                }
+                else if (Choice == 4)
+                {
+                    Console.Write("\nHow much would you like to Withdraw: ");
+                    string input = Console.ReadLine();
+                    int value = Convert.ToInt32(input);
+                    if (value! < Balance)
+                    {
+                        Console.Clear();
+                        Balance = Balance - value;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You dont have enough balance to Withdraw: " + value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ATM V2/ATM V2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Net.NetworkInformation;
+using System.Xml.Serialization;
+
+namespace ATM_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region CardHolder
+
+            Cardholder cardholder1 = new Cardholder();
+            cardholder1.Name = "Einars";
+            cardholder1.Lastname = "Lapins";
+            cardholder1.Pin = 1234;
+            cardholder1.Balance = 100;
+
+            #endregion
+
+            #region Variables
+            int tries = 1;
+            int maxattempts = 3;
+            int Choice = 0;
+            #endregion
+
+            #region ATM
+            bool ATM = true ;
+            do
+            {
+                Mainmenu();
+                
+                int pin = ReadInt("Input:");
+
+                Console.Clear();
+                
+                if (cardholder1.Pin == pin)            
+                {
+                    cardholder1.getBalance();
+                    do
+                    {
+                        secondmenu();
+
+                        Choice = ReadInt("Input:");
+
+                        if (Choice == 1)
+                        {
+                            cardholder1.doDeposit();
+                            Console.Clear();
+                            cardholder1.getBalance();
+                        }
+                        else if (Choice == 2)
+                        {
+                            cardholder1.doWithdraw();
+                            cardholder1.getBalance();
+                        }
+
+                    } while (Choice != 3);
+                    Console.Clear();
+                }
+                else if (cardholder1.Pin != pin)
+                {
+                    if (tries == maxattempts)
+                    {
+                        ATM = false;
+                    }
+                    Console.Clear();
+                    Console.WriteLine($"Wrong PIN, You have: {maxattempts - tries} Tries Left");
+                    tries++;
+                }
+
+            } while (ATM);
+            #endregion
+        }
+        static void Mainmenu()
+        {
+            Console.WriteLine("---Insert Your Card---");
+
+            for (int i = 0; i < 25; i++) 
+            {
+                Console.Write("-"); Task.Delay(1).Wait();  //speed 45
+            }
+            Console.WriteLine("\nEnter Your 4 Digit PIN Number");
+        }
+        static void secondmenu()
+        {
+            Console.WriteLine("\n1# Deposit");
+            Console.WriteLine("\n2# Withdraw");
+            Console.WriteLine("\n3# Exit to Main Menu");
+            Console.WriteLine("-------------------");
+        }
+        static int ReadInt(string promt)
+        {
+            Console.Write(promt);
+            string input = Console.ReadLine();
+            int value = Convert.ToInt32(input);
+            return value;
+        }
+    }
+}
